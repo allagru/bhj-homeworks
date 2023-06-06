f0ca66e46b86b9e132a66c5302c5d420f9a46437
Alla <liliya86@inbox.ru>
2023-06-07 00:29:39 +0300

    fix menu task

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,6 @@
-const menuSub = Array.from(document.querySelectorAll('.menu_sub'));
-
-menuSub.forEach(item => {
-    const menuLink = item.parentElement.querySelector('.menu__link');
-
-    menuLink.onclick = () => {
-        menuSub.forEach((list) => {
-            if (item.closest('.menu_main') === list.closest('.menu_main')) {
-                list.classList.remove('menu_active')
-            }
-        })
-        item.classList.add('menu_active');
+document.querySelector(".menu_main").onclick = (event) => {
+    if (event.target.nextElementSibling) {
+        event.target.nextElementSibling.classList.toggle("menu_active");
         return false;
     }
-});
+};
